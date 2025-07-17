1afc888cca84799dcab857aa4736ec69669ea3d8
HadassaPizani <hadapizani@gmail.com>
2025-07-17 10:31:57 -0300

    Error corregido en la conexion base de datos

--- config.php
+++ config.php
@@ -1,27 +1,8 @@
 <?php
-class Database
-{
-    private $host = 'localhost';
-    private $dbname = 'postgres';
-    private $username = 'postgres';
-    private $password = 'prueba1234';
-    private $connection;
-
-    public function connect()
-    {
-        $this->connection = null;
-
-        try {
-            $this->connection = new PDO(
-                "pgsql:host=" . $this->host . ";dbname=" . $this->dbname,
-                $this->username,
-                $this->password
-            );
-            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-            echo "Error de conexión: " . $e->getMessage();
-        }
-
-        return $this->connection;
-    }
-}
+// realizamos conexion a la base de datos
+return [
+    'db_host' => 'localhost',
+    'db_name' => 'postgres',
+    'db_user' => 'postgres',
+    'db_pass' => 'prueba1234',
+];
